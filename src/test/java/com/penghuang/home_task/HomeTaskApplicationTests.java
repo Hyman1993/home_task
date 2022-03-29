08f5673caabcf5cd9c0670c12a580fd136599029
Hyman1993 <1392841128@qq.com>
2022-03-29 23:31:42 +0900

    2022.03.29 init project

---
+++ src/test/java/com/penghuang/home_task/HomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penghuang.home_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
